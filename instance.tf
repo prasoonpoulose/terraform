0de94b548f0f5a044c86fdc926be71270f819b89
prasoonpoulose <104289471+prasoonpoulose@users.noreply.github.com>
2022-08-06 09:27:57 +0530

    First Commit

---
+++ instance.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region  = "${var.aws_region}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {
+    default = "us-east-1"
+}
+
+resource "aws_instance" "Terraform-lab" {
+  count = "1"
+  ami           = "ami-090fa75af13c156b4"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  key_name= "rhel"
+  tags = {
+    Name = "terraform"
+  }
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo amazon-linux-extras install ansible2
+    ansible --version
+  EOF
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+aws_access_key =  "AKIAX7TLY5OHDU45V74Z"
+aws_secret_key =  "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"
+aws_region = "us-east-1"
